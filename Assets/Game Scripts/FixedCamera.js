f6a9d3126c42581ddff7ca9c5fb0aed390fa9b72
Josh L <funinvegas@gmail.com>
2014-11-23 22:43:45 -0800

    Adding rudamentry game board

---
+++ Assets/Game Scripts/FixedCamera.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+	Screen.autorotateToLandscapeLeft = false;
+	Screen.autorotateToPortrait = true;
+	Screen.autorotateToLandscapeRight = false;
+	Screen.autorotateToPortraitUpsideDown = true;
+	Screen.orientation = ScreenOrientation.Portrait;
+	Screen.SetResolution(320,640, true);
+	Camera.main.orthographicSize = 320;
+	Camera.main.transform.position = new Vector3(320/2, -640/2, -10);
+
+}
+
+function Update () {
+
+}
